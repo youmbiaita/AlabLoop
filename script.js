b81326088bc61a21302077cc8042020faa7bb5e0
Tchakounteaita <164936530+youmbiaita@users.noreply.github.com>
2024-04-12 22:28:14 -0400

    added part2

---
+++ script.js
@@ -0,0 +1,28 @@
+// Part 1: Fizz Buzz
+let i = 1;
+while (i <= 100) {
+    if (i % 5 === 0 && i % 3 === 0) {
+        console.log("FIZZ BUZZ")
+    } else if (i % 3 === 0) {
+        console.log("FIZZ");
+    } else if (i % 5 === 0) {
+        console.log("BUZZ")
+    } else {
+        console.log(i)
+    }
+    i++
+}
+
+console.log("==========================================================")
+console.log("==========================================================")
+
+let n = 2;
+let nextPrime = n + 1;
+let notPrime = true;
+
+while(isPrime)
+for (let j = 2 ; j < nextPrime; j++ ) {
+    if (nextPrime % j === 0) {
+       break;
+    }
+}

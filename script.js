88f83d1ae7da06fdce440c1a1f4194402f52eb6c
Tchakounteaita <164936530+youmbiaita@users.noreply.github.com>
2024-04-14 18:06:25 -0400

    added changes on part3

--- script.js
+++ script.js
@@ -40,9 +40,11 @@ while(notPrime) {
 
 let characters = "\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
 let arrayCharacter = [];
+let newLine = "\n";
+console.log(characters.substring(0, 2));
 for (let k = 0; k < characters.length; k++) {
-    if (characters.charAt(k) === '\\' && characters.charAt(k + 1) === 'n') {
-        
+    if (characters.substring(k, k+1) === newLine) {
+
 
     }
 }

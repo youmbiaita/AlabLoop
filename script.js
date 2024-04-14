ce26bbaf1b3434445dd706b81fc070b3f32fa2d4
Tchakounteaita <164936530+youmbiaita@users.noreply.github.com>
2024-04-14 12:51:01 -0400

    added changes

--- script.js
+++ script.js
@@ -16,13 +16,38 @@ while (i <= 100) {
 console.log("==========================================================")
 console.log("==========================================================")
 
-let n = 2;
+// Part2 : Prime Time
+
+let n = 25;
 let nextPrime = n + 1;
 let notPrime = true;
 
-while(isPrime)
-for (let j = 2 ; j < nextPrime; j++ ) {
-    if (nextPrime % j === 0) {
-       break;
+while(notPrime) {
+    let prime = nextPrime;
+    for (let j = 2 ; j < nextPrime; j++ ) {
+        if (nextPrime % j === 0) {
+            nextPrime++;
+           break;
+        }
+    }
+    if (prime === nextPrime) {
+        notPrime = false;
+        console.log(`The next prime after ${n} is ${nextPrime}`)
     }
 }
+
+// Part 3: Feeling Loopy
+
+let characters = "\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
+let arrayCharacter = [];
+for (let k = 0; k < characters.length; k++) {
+    if (characters.charAt(k) === '\\' && characters.charAt(k + 1) === 'n') {
+        
+
+    }
+}
+
+
+
+
+

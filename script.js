5b2a434f9dbb57678fb7b68ffb9916e6e39a87b9
Tchakounteaita <164936530+youmbiaita@users.noreply.github.com>
2024-04-14 22:28:02 -0400

    some changed but not done

--- script.js
+++ script.js
@@ -38,17 +38,29 @@ while(notPrime) {
 
 // Part 3: Feeling Loopy
 
-let characters = "\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
-let arrayCharacter = [];
-let newLine = "\n";
-console.log(characters.substring(0, 2));
-for (let k = 0; k < characters.length; k++) {
-    if (characters.substring(k, k+1) === newLine) {
-
-
+let characters = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+let arrayCharactes = [];
+let str = characters.charAt(0);
+let row = [];
+for (let k = 1; k < characters.length; k++) {
+    if (characters.substring(k - 1, k) === "\n") {
+        continue;
+    } else if (characters.substring(k - 2, k - 1) === "\n") {
+        row.push(str);
+        arrayCharactes.push(row);
+        row = [];
+    } else {
+        if (characters.charAt(k) === ",") {
+            if (characters.substring(k - 1, k) !== "\n") {
+                str += characters.charAt(k);
+            } else {
+                row.push(str);
+                str = "";
+            }
+        }
     }
 }
-
+console.log(arrayCharactes);
 
 
 
